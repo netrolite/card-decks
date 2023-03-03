3ed3e30fc4cf3151123e68af1d669b137b94a4be
Matvey <morozovmatvej75@gmail.com>
2023-03-03 19:34:30 +0300

    add await keyword

--- server/src/controllers/decks.ts
+++ server/src/controllers/decks.ts
@@ -2,7 +2,7 @@ import { Request, Response } from "express";
 import Deck from "../models/Deck";
 
 export async function getDecks(req: Request, res: Response) {
-  const decks = Deck.find({});
+  const decks = await Deck.find({});
   res.status(200).json(decks);
 }
 
