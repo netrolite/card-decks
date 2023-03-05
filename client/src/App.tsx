277804e0398268db0ade2d7c79ded01a32f47441
Matvey <morozovmatvej75@gmail.com>
2023-03-05 15:38:44 +0300

    receive decks from backend

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,19 +1,38 @@
-import { useEffect } from "react";
+import { useState, useEffect } from "react";
 import axios from "axios";
 
 axios.defaults.baseURL = "http://localhost:4000"
 
+interface IDeck {
+  name: string
+}
+
 const App = () => {
+  const [decks, setDecks] = useState<IDeck[]>([])
+
   useEffect(() => {
     (async () => {
-      const data = await axios("/decks");
-      console.log(data);
+      setDecks(await fetchDecks());
     })();
   }, [])
+  console.log(decks);
 
   return (
-    <h1>hello</h1>
+    <div className="app">
+      <div className="decks">
+        { }
+      </div>
+    </div>
   )
+}
+
+async function fetchDecks() {
+  try {
+    const { data } = await axios.get("/decks");
+    return data;
+  } catch (err) {
+    console.error(err);
+  }
 }
 
 export default App;
